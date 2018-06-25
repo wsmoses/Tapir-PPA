bc51b6c73cd695aeff5923de60ef145f845215ae
William S. Moses <gh@wsmoses.com>
2018-06-25 12:42:23 -0400

    addl

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -7,7 +7,7 @@ set -o xtrace
 # To create an rc1 release:
 # sh 4.0/debian/orig-tar.sh RELEASE_40 rc1
 
-PACKAGE_NAME="tapir-toolchain-5.0"
+PACKAGE_NAME="tapir-toolchain"
 MAJOR_VERSION=5.0
 LLVM_VER="release_50"
 
@@ -51,9 +51,9 @@ update_source compiler-rt https://github.com/wsmoses/Tapir-Compiler-RT
 update_source polly https://github.com/wsmoses/Tapir-Polly
 update_source lld https://github.com/llvm-mirror/lld $LLVM_VER
 
-EXACT_VERSION=~`cd $SVN_ARCHIVES/llvm && git rev-parse HEAD`
-VERSION=$MAJOR_VERSION$EXACT_VERSION
-FULL_VERSION="$PACKAGE_NAME-"$VERSION
+EXACT_VERSION=`cd $SVN_ARCHIVES/llvm && git rev-parse HEAD`
+VERSION=$MAJOR_VERSION~git$EXACT_VERSION
+FULL_VERSION="$PACKAGE_NAME"_"$VERSION"
 MED_VERSION="$PACKAGE_NAME-"$MAJOR_VERSION
 
 compress_source() {
@@ -92,7 +92,8 @@ export DEBEMAIL="deb@wsmoses.com"
 cd $PATH_DEBIAN
 
 cd $dir
-dch --distribution xenial #$EXTRA_DCH_FLAGS --distribution $DISTRIBUTION --newversion 1:$VERSION-1~exp1 "New snapshot release"
+dch --distribution experimental --newversion 1:$VERSION-1~exp1 "New snapshot release"
+#$EXTRA_DCH_FLAGS --distribution $DISTRIBUTION --newversion 1:$VERSION-1~exp1 "New snapshot release"
 
 tar jxf ../$FULL_VERSION.orig.tar.bz2 --strip-components=1
 for f in clang compiler-rt polly clang-tools-extra lld; do
